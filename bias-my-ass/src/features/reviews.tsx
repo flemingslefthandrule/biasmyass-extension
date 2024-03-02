1a9b91a3b7574e333c0f8bdf2f703d5057874478
MGuruNikhil <gurunikhilmangaraju@gmail.com>
2024-03-02 21:01:56 +0530

    allComBasic

---
+++ bias-my-ass/src/features/reviews.tsx
@@ -0,0 +1,9 @@
+export const Reviews = () => {
+    return (
+        <div className="plasmo-border-solid plasmo-border-black plasmo-border-2 plasmo-rounded-md plasmo-overflow-y-auto plasmo-p-2">
+            <div className="plasmo-border-b-solid plasmo-border-b-black plasmo-border-b-2">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem nesciunt nobis, laudantium porro quod ullam voluptatum? Maiores dolor impedit optio dolorum nostrum cum. Sint quo temporibus voluptate sit nisi laborum.</div>
+            <div className="plasmo-border-b-solid plasmo-border-b-black plasmo-border-b-2">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium delectus ratione consequatur veniam, quasi obcaecati expedita incidunt impedit. Voluptatibus voluptates consectetur expedita eveniet eaque amet consequatur autem quam deleniti sint?</div>
+            <div className="plasmo-border-b-solid plasmo-border-b-black plasmo-border-b-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam ad eveniet tenetur nobis esse fuga magnam nesciunt impedit exercitationem corporis magni facere, molestiae sapiente totam eius asperiores rerum vitae enim?</div>
+        </div>
+    )
+}
--- bias-my-ass/src/popup.tsx
+++ bias-my-ass/src/popup.tsx
@@ -1,12 +1,17 @@
-
-
 import { AuthorDetails } from "~features/author"
+import { Reviews } from "~features/reviews"
+import { Summery } from "~features/summary"
 import "~style.css"
 
 function IndexPopup() {
   return (
-    <div className="plasmo-flex plasmo-items-center plasmo-justify-center plasmo-h-[600px] plasmo-w-72 plasmo-rounded-lg">
+    <div className="plasmo-p-2 plasmo-flex plasmo-flex-col plasmo-gap-3 plasmo-justify-center plasmo-items-center plasmo-h-[600px] plasmo-w-72 plasmo-rounded-lg">
+      <p className="plasmo-font-bold plasmo-text-xl ">BiasMyAss</p>
       <AuthorDetails />
+      <Summery />
+      <p className="plasmo-font-bold plasmo-text-lg">Reviews</p>
+      <Reviews />
+      <a href="#">For more details</a>
     </div>
   )
 }
