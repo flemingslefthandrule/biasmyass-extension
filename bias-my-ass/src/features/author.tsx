3216d299d24638e04ab6b64005df54542816eadc
MGuruNikhil <gurunikhilmangaraju@gmail.com>
2024-03-02 06:56:58 +0530

    authorStyling

---
+++ bias-my-ass/src/features/author.tsx
@@ -0,0 +1,14 @@
+import { useReducer } from "react"
+
+export const AuthorDetails = () => {
+
+  return (
+    <div className="plasmo-max-h-[56px] plasmo-flex plasmo-flex-row plasmo-p-2 plasmo-gap-x-2 plasmo-border-solid plasmo-border-black plasmo-border-2 plasmo-rounded-md plasmo-overflow-hidden plasmo-cursor-pointer">
+      <img className="plasmo-rounded-[50%] plasmo-object-cover" src={require("../../assets/Dummy.png")} alt="pp" width={'40px'} height={'40px'} />
+      <div className="plasmo-info plasmo-flex plasmo-flex-col plasmo-items-start plasmo-justify-center">
+        <span className="plasmo-font-bold plasmo-text-lg plasmo-text-left">Author Name</span>
+        <p className="plasmo-text-xs plasmo-text-left">working as</p>
+      </div>
+    </div>
+  )
+}
--- bias-my-ass/src/popup.tsx
+++ bias-my-ass/src/popup.tsx
@@ -1,11 +1,12 @@
-import { CountButton } from "~features/count-button"
 
+
+import { AuthorDetails } from "~features/author"
 import "~style.css"
 
 function IndexPopup() {
   return (
-    <div className="plasmo-flex plasmo-items-center plasmo-justify-center plasmo-h-16 plasmo-w-40">
-      <CountButton />
+    <div className="plasmo-flex plasmo-items-center plasmo-justify-center plasmo-h-[600px] plasmo-w-72 plasmo-rounded-lg">
+      <AuthorDetails />
     </div>
   )
 }
