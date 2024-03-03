27bddc439d13bd103a171c6b198708d25bdc9b49
sam <aquaticcalf@proton.me>
2024-03-03 21:06:12 +0530

    meow

--- src/features/reviews.tsx
+++ src/features/reviews.tsx
@@ -1,9 +1,21 @@
+import test from "node:test"
+import { useState } from "react"
+
 export const Reviews = () => {
+    const testReviews = [
+        "“I recently had a consultation with this lawyer and it was very helpful – they were knowledgeable and professional, and provided valuable advice. I would definitely recommend them.”",
+        "“I visited this amusement park and it was a blast – the rides were thrilling and the atmosphere was festive. It was a great day out.”",
+        "“I had a great experience with this online marketplace – the selection was impressive and the prices were competitive. The customer service was also excellent.”",
+        "“I recently had a photoshoot with this photographer and it was amazing – they were skilled and creative, and the results were beautiful. The price was also very reasonable.”"
+    ]
+
+    const [reviewslist, setReviewslist] = useState(testReviews)
+
     return (
         <div className="border-solid border-white scrollbar border-2 rounded-md overflow-y-auto p-2">
-            <div className="p-2 border-b-solid border-b-white border-b-2">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem nesciunt nobis, laudantium porro quod ullam voluptatum? Maiores dolor impedit optio dolorum nostrum cum. Sint quo temporibus voluptate sit nisi laborum.</div>
-            <div className="p-2 border-b-solid border-b-white border-b-2">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium delectus ratione consequatur veniam, quasi obcaecati expedita incidunt impedit. Voluptatibus voluptates consectetur expedita eveniet eaque amet consequatur autem quam deleniti sint?</div>
-            <div className="p-2 border-b-solid border-b-white border-b-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam ad eveniet tenetur nobis esse fuga magnam nesciunt impedit exercitationem corporis magni facere, molestiae sapiente totam eius asperiores rerum vitae enim?</div>
+                {reviewslist.map((review) => (
+                    <div key={review} className="p-2 border-b-solid border-b-white border-b-2">{review}</div>
+                ))}
         </div>
     )
 }
