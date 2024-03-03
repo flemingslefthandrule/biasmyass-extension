12307ad01f98cf9156ba369ba020debc1472a589
sam <aquaticcalf@proton.me>
2024-03-03 20:12:23 +0530

    meow

--- src/content.tsx
+++ src/content.tsx
@@ -14,12 +14,12 @@ export const getStyle = () => {
 
 const PlasmoOverlay = () => {
   const { data } = useMessage<string, string>(async (req, res) => {
-    const authorName = document.querySelector('a[data-testid="authorName"]').textContent;
-    const authorPhoto = document.querySelector('img[data-testid="authorPhoto"]');
-    const authorPhotoURL = authorPhoto.getAttribute('src');
-    const response = authorName+","+authorPhotoURL;
-    res.send(response);
-  });
+    const authorName = document.querySelector('a[data-testid="authorName"]').textContent
+    const authorPhoto = document.querySelector('img[data-testid="authorPhoto"]')
+    const authorPhotoURL = authorPhoto.getAttribute('src')
+    const response = authorName+","+authorPhotoURL
+    res.send(response)
+  })
 }
 
 export default PlasmoOverlay
