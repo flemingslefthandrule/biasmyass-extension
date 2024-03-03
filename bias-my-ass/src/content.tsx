8dd929a93aed2f4f2115d83940fd187d844ee6f9
MGuruNikhil <gurunikhilmangaraju@gmail.com>
2024-03-03 14:01:25 +0530

    img success

--- bias-my-ass/src/content.tsx
+++ bias-my-ass/src/content.tsx
@@ -14,9 +14,11 @@ export const getStyle = () => {
 
 const PlasmoOverlay = () => {
   const { data } = useMessage<string, string>(async (req, res) => {
-    if(req.name="sent author name") {
-      res.send(document.querySelector('a[data-testid="authorName"]').textContent);
-    }
+    const authorName = document.querySelector('a[data-testid="authorName"]').textContent;
+    const authorPhoto = document.querySelector('img[data-testid="authorPhoto"]');
+    const authorPhotoURL = authorPhoto.getAttribute('src');
+    const response = authorName+","+authorPhotoURL;
+    res.send(response);
   });
 }
 
--- bias-my-ass/src/features/author.tsx
+++ bias-my-ass/src/features/author.tsx
@@ -3,14 +3,17 @@ import { sendToContentScript } from "@plasmohq/messaging"
 
 export const AuthorDetails = () => {
   const [authorName, setAuthorName] = useState("Author Name");
+  const [authorPhoto, setAuthorPhoto] = useState(require("../../assets/Dummy.png"));
 
   const receiveAuthorName = async () => {
     const resp = await sendToContentScript({name:"authorName"});
-    setAuthorName(resp)
+    const authorDetails = resp.split(',');
+    setAuthorName(authorDetails[0]);
+    setAuthorPhoto(authorDetails[1]);
   }
   return (
     <div onLoad={receiveAuthorName} className="plasmo-max-h-[56px] plasmo-flex plasmo-flex-row plasmo-p-2 plasmo-gap-x-2 plasmo-border-solid plasmo-border-black plasmo-border-2 plasmo-rounded-md plasmo-overflow-hidden plasmo-cursor-pointer">
-      <img className="plasmo-rounded-[50%] plasmo-object-cover" src={require("../../assets/Dummy.png")} alt="pp" width={'40px'} height={'40px'} />
+      <img className="plasmo-rounded-[50%] plasmo-object-cover" src={authorPhoto} alt="authorPhoto" width={'40px'} height={'40px'} />
       <div className="plasmo-info plasmo-flex plasmo-flex-col plasmo-items-start plasmo-justify-center">
         <span className="plasmo-font-bold plasmo-text-lg plasmo-text-left">{ authorName }</span>
         <p className="plasmo-text-xs plasmo-text-left">working as</p>
