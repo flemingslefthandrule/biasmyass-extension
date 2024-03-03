918d0e52b0ebc251e7b2e63d40ca838d28527531
sam <aquaticcalf@proton.me>
2024-03-03 21:23:43 +0530

    meow

--- src/features/author.tsx
+++ src/features/author.tsx
@@ -5,7 +5,7 @@ export const AuthorDetails = () => {
   const [authorName, setAuthorName] = useState("")
   const [authorUrl, setAuthorUrl] = useState("")
   const [authorPhoto, setAuthorPhoto] = useState(require("../../assets/test.webp"))
-  const [authorWork, setAuthorWork] = useState("")
+  const [authorWork, setAuthorWork] = useState("test")
 
   const receiveAuthorName = async () => {
     try {
@@ -22,9 +22,9 @@ export const AuthorDetails = () => {
   return (
     <a href={authorUrl} target="_blank" onLoad={receiveAuthorName} className="max-h-[56px] flex justify-center items-center p-2 gap-x-2 border-solid border-white border-2 rounded-md overflow-hidden cursor-pointer">
       <img className="rounded-[50%] object-cover w-[35px] h-[35px] max-w-[35px] max-h-[35px]" src={authorPhoto} alt="authorPhoto" />
-      <div className="info flex flex-col items-start justify-center">
-        <span className="font-bold text-lg text-left">{authorName}</span>
-        <div className="flex">
+      <div className="info flex flex-col items-start gap-0 py-3">
+        <span className="font-bold text-lg">{authorName}</span>
+        <div className="text-gray-300">
           <p id="work">{authorWork}</p>
         </div>
       </div>
