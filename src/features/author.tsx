362b70340fc56ea6340bd42ef3db8810030fb30b
Nischal_UwU <118107697+Nischa1Mv@users.noreply.github.com>
2024-03-05 14:53:03 +0530

    uwu

--- src/features/author.tsx
+++ src/features/author.tsx
@@ -1,20 +1,24 @@
-import { useState } from "react"
-import { sendToContentScript } from "@plasmohq/messaging"
-
 export const AuthorDetails = (props) => {
-  const authorName = props.authorName;
-  const authorUrl = props.authorUrl;
-  const authorPhoto = props.authorPhoto;
-  const authorWork = props.authorWork;
+  const authorName = props.authorName
+  const authorUrl = props.authorUrl
+  const authorPhoto = props.authorPhoto
+  const authorWork = props.authorWork
   return (
-    <a href={authorUrl} target="_blank" onLoad={receiveAuthorName} className="max-h-[56px] flex justify-center items-center p-2 gap-x-2 border-solid border-white border-2 rounded-md overflow-hidden cursor-pointer">
-      <img className="rounded-[50%] object-cover w-[35px] h-[35px] max-w-[35px] max-h-[35px]" src={authorPhoto} alt="authorPhoto" />
-      <div className="info flex flex-col items-start gap-0 py-3">
-        <span className="font-bold text-lg">{authorName}</span>
-        <div className="text-gray-300">
-          <p id="work">{authorWork}</p>
-        </div>
-      </div>
-    </a>
+      <a
+          href={authorUrl}
+          target="_blank"
+          className="max-h-[56px] flex justify-center items-center p-2 gap-x-2 border-solid border-white border-2 rounded-md overflow-hidden cursor-pointer">
+          <img
+              className="rounded-[50%] object-cover w-[35px] h-[35px] max-w-[35px] max-h-[35px]"
+              src={authorPhoto}
+              alt="authorPhoto"
+          />
+          <div className="info flex flex-col items-start gap-0 py-3">
+              <span className="font-bold text-lg">{authorName}</span>
+              <div className="text-gray-300">
+                  <p id="work">{authorWork}</p>
+              </div>
+          </div>
+      </a>
   )
 }
--- src/popup.tsx
+++ src/popup.tsx
@@ -1,36 +1,62 @@
+import { useState } from "react"
+
+import { sendToContentScript } from "@plasmohq/messaging"
+
 import { AuthorDetails } from "~features/author"
 import { Reviews } from "~features/reviews"
 import { Summary } from "~features/summary"
+
 import "~style.css"
 
 function IndexPopup() {
-  const [authorName, setAuthorName] = useState("")
-  const [authorUrl, setAuthorUrl] = useState("")
-  const [authorPhoto, setAuthorPhoto] = useState(require("../assets/test.webp"))
-  const [authorWork, setAuthorWork] = useState("journalist")
+    const [authorName, setAuthorName] = useState("")
+    const [authorUrl, setAuthorUrl] = useState("")
+    const [authorPhoto, setAuthorPhoto] = useState(
+        require("../assets/test.webp")
+    )
+    const [authorWork, setAuthorWork] = useState("journalist")
 
-  const receiveAuthorName = async () => {
-    try {
-      const resp = await sendToContentScript({ name: "authorName" })
-      const authorDetails = resp.split(",")
-      setAuthorName(authorDetails[0])
-      setAuthorPhoto(authorDetails[1])
-      setAuthorUrl(authorDetails[2])
-    } catch (error) {
-      setAuthorName("website not supported")
-      setAuthorUrl("https://google.com/search?q=biasmyasswebsitenotsupportedurl")
+    const receiveAuthorName = async () => {
+        try {
+            const resp = await sendToContentScript({ name: "authorName" })
+            const authorDetails = resp.split(",")
+            setAuthorName(authorDetails[0])
+            setAuthorPhoto(authorDetails[1])
+            setAuthorUrl(authorDetails[2])
+        } catch (error) {
+            setAuthorName("website not supported")
+            setAuthorUrl(
+                "https://google.com/search?q=biasmyasswebsitenotsupportedurl"
+            )
+        }
     }
-  }
-  return (
-    <div className="bg-black text-white flex flex-col gap-3 justify-center items-center h-[600px] w-[300px] px-[10px] py-[10px] rounded-lg">
-      <a href="https://google.com/search?q=biasmyass" target="_blank" className="font-bold text-xl hover:text-green-300">BiasMyAss</a>
-      <AuthorDetails authorName={authorName} authorUrl={authorUrl} authorPhoto={authorPhoto} authorWork={authorWork}/>
-      <Summary />
-      <p className="font-bold text-lg">Reviews</p>
-      <Reviews />
-      <a href="https://cdn.tailwindcss.com" target="_blank" className="underline">For more details</a>
-    </div>
-  )
+    return (
+        <div
+            onLoad={receiveAuthorName}
+            className="bg-black text-white flex flex-col gap-3 justify-center items-center h-[600px] w-[300px] px-[10px] py-[10px] rounded-lg">
+            <a
+                href="https://google.com/search?q=biasmyass"
+                target="_blank"
+                className="font-bold text-xl hover:text-green-300">
+                BiasMyAss
+            </a>
+            <AuthorDetails
+                authorName={authorName}
+                authorUrl={authorUrl}
+                authorPhoto={authorPhoto}
+                authorWork={authorWork}
+            />
+            <Summary />
+            <p className="font-bold text-lg">Reviews</p>
+            <Reviews />
+            <a
+                href="https://cdn.tailwindcss.com"
+                target="_blank"
+                className="underline">
+                For more details
+            </a>
+        </div>
+    )
 }
 
 export default IndexPopup
