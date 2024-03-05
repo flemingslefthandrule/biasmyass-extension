3ad603d44f6119c8be1632a5a0d26d9d0f9eaa4a
M Guru Nikhil <130775801+MGuruNikhil@users.noreply.github.com>
2024-03-05 12:08:31 +0530

    content to popup

--- src/features/author.tsx
+++ src/features/author.tsx
@@ -1,24 +1,11 @@
 import { useState } from "react"
 import { sendToContentScript } from "@plasmohq/messaging"
 
-export const AuthorDetails = () => {
-  const [authorName, setAuthorName] = useState("")
-  const [authorUrl, setAuthorUrl] = useState("")
-  const [authorPhoto, setAuthorPhoto] = useState(require("../../assets/test.webp"))
-  const [authorWork, setAuthorWork] = useState("journalist")
-
-  const receiveAuthorName = async () => {
-    try {
-      const resp = await sendToContentScript({ name: "authorName" })
-      const authorDetails = resp.split(",")
-      setAuthorName(authorDetails[0])
-      setAuthorPhoto(authorDetails[1])
-      setAuthorUrl(authorDetails[2])
-    } catch (error) {
-      setAuthorName("website not supported")
-      setAuthorUrl("https://google.com/search?q=biasmyasswebsitenotsupportedurl")
-    }
-  }
+export const AuthorDetails = (props) => {
+  const authorName = props.authorName;
+  const authorUrl = props.authorUrl;
+  const authorPhoto = props.authorPhoto;
+  const authorWork = props.authorWork;
   return (
     <a href={authorUrl} target="_blank" onLoad={receiveAuthorName} className="max-h-[56px] flex justify-center items-center p-2 gap-x-2 border-solid border-white border-2 rounded-md overflow-hidden cursor-pointer">
       <img className="rounded-[50%] object-cover w-[35px] h-[35px] max-w-[35px] max-h-[35px]" src={authorPhoto} alt="authorPhoto" />
